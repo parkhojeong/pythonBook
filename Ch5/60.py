6d717fcdf87a4a66c9feb873546d7f92c08b0de1
phj <parkhj062@gmail.com>
2018-11-12 00:30:33 +0900

    5-3완료

---
+++ Ch5/60.py
@@ -0,0 +1,23 @@
+import pickle
+def main():
+    citiesDict = createDictFromBinaryFile("LargeCitiesDict.dat")
+    print(citiesDict)
+    stateName = inputStr()
+    printLargeStates(citiesDict,stateName)
+
+def createDictFromBinaryFile(filename):
+    infile = open(filename,"rb")
+    return pickle.load(infile)
+
+def inputStr():
+    return input("Enter the name of a state: ")
+
+def printLargeStates(citiesDict, stateName):
+    if citiesDict[stateName]:
+        print("Large cities: ",end="")
+        for item in citiesDict[stateName]:
+            print(item,end=" ")
+    else:
+        print("There are no large cities in",stateName)
+
+main()
