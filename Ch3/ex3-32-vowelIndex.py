f180d98e33495c9811ea9a0b94652c49bbcfbb79
phj <parkhj062@gmail.com>
2018-12-05 13:47:30 +0900

    update

--- Ch3/ex3-32-vowelIndex.py
+++ Ch3/ex3-32-vowelIndex.py
@@ -1,4 +1,4 @@
 str= input("Enter word to translate: ")
-index = [i for i in range(len(str)) if str[i] in ['a', 'e', 'i', 'o', 'u'] ]
+index = [i for i in range(len(str)) if str[i] in "aeiou" ]
 print(index[0])
 
--- Ch5/proj5_6.py
+++ Ch5/proj5_6.py
@@ -1,19 +1,18 @@
-file = open("Mileage.txt")
-mDict = [[line.split(',')[0],eval(line.split(',')[1].rstrip())] for line in file ]
+inputList = [[line.split(',')[0],eval(line.split(',')[1].rstrip())] for line in open("Mileage.txt") ]
 resDict ={}
-# print(mDict)
-for i in range(len(mDict)):
-    item = mDict[i]
+print(inputList)
+for i in range(len(inputList)):
+    item = inputList[i] # 한줄 씩 item으로 저장 후 사용
     key = item[0]
     if resDict.get(key,-1) == -1:
-        resDict[key] = (1,mDict[i][1])
+        resDict[key] = {"cnt":1,"sumGallon":item[1]}
     else:
-        sumGallon = resDict[key][0] * resDict[key][1]+ mDict[i][1]
-        cnt = resDict[key][0]+1
-        avgGallon = sumGallon /cnt
-        resDict[key] = (cnt,avgGallon)
+        resDict[key]["cnt"]+=1
+        resDict[key]["sumGallon"] = resDict[key]["sumGallon"]+item[1]
+
+    resDict[key]["MPG"] = resDict[key]["cnt"] * 100 / resDict[key]["sumGallon"]
 
 print("{0:<15}{1:<15}".format("Model","MPG"))
-for item in sorted(resDict.items(),key = lambda k : k[1][1],reverse= True):
+for item in sorted(resDict.items(),key = lambda k : k[1]["MPG"],reverse= True):
     key = item[0]
-    print("{0:<15s}{1:<15.2f}".format(key,resDict[key][1]))
+    print("{0:<15s}{1:<15.2f}".format(key,resDict[key]["MPG"]))
